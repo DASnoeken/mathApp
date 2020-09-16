7b39eca1805dd0859e1e48154ecf1295f12b533b
Daan Snoeken <d.a.snoeken@protonmail.com>
2020-09-16 16:14:44 +0200

    basic calculator works

--- src/main/java/nodatabase/test/domain/Calculator.java
+++ src/main/java/nodatabase/test/domain/Calculator.java
@@ -21,14 +21,14 @@ public class Calculator {
 	}
 	
 	public Calculator(String input) {
-		this.sum = "1*1-1+"+input+"*1"; //Shut up! This works (for now)!
+		this.sum = input; 
 		this.answer = 0.0;
 		calculate();
 	}
 	public void calculate() {
 		this.sum = this.sum.replace("\\s", "");					//remove spaces
-		this.terms = this.sum.split("\\+|\\-|\\*|\\/");			//contains numbers
-		this.ops = this.sum.split("\\d{1,}");					//contains operations
+		this.terms = this.sum.split("\\+|\\-");					//contains numbers
+		this.ops = this.sum.split("[\\*\\/]?\\d{1,}[\\*\\/]?");					//contains operations
 		this.addsubTerms = new ArrayList<Double>();
 		boolean test = Arrays.stream(ops).anyMatch(""::equals);
 		if(test) {	//Remove annoying empty strings
@@ -40,27 +40,34 @@ public class Calculator {
 			ops = new String[help.size()];
 			ops = help.toArray(ops);
 		}
-		//multiply and divide first
-		for(int i=0;i<ops.length;i++){
-			if(ops[i].equals("*")) {
-				addsubTerms.add(multiply(Double.parseDouble(terms[i]),Double.parseDouble(terms[i+1])));
-			}else if(ops[i].equals("/")) {
-				try {
-					addsubTerms.add(divide(Double.parseDouble(terms[i]),Double.parseDouble(terms[i+1])));
-				}catch(ArithmeticException ae) {
-					this.answer = Double.NaN;
-					return;
-				}
-			}else if(((i==ops.length-1) && ops[i].equals("+")) || ops[i].equals("+")&&!(ops[i+1].equals("*")||ops[i+1].equals("/"))) {
-				addsubTerms.add(Double.parseDouble(terms[i]));
-			}else if(((i==ops.length-1) && ops[i].equals("-")) || ops[i].equals("-")&&!(ops[i+1].equals("*")||ops[i+1].equals("/"))) {
+		//multiply and divide first	
+		for(int i=0;i<terms.length;i++) {
+			Double finalValue = 1.0;
+			if(!terms[i].contains("*") && !terms[i].contains("/")) {
 				addsubTerms.add(Double.parseDouble(terms[i]));
 			}
+			String[] termsI = terms[i].split("\\*|\\/");
+			finalValue*=Double.parseDouble(termsI[0]);
+			String[] opsI = terms[i].split("\\d{1,}");
+			boolean test2 = Arrays.stream(opsI).anyMatch(""::equals);
+			if(test2) {	//Remove annoying empty strings
+				ArrayList<String> help = new ArrayList<>();
+				for(int q = 0;q<opsI.length;q++) {
+					if(!opsI[q].equals(""))
+						help.add(opsI[q]);
+				}
+				opsI = new String[help.size()];
+				opsI = help.toArray(opsI);
+			}
+			for(int j=0;j<opsI.length;j++) {
+				if(opsI[j].equals("*")) {
+					finalValue*=Double.parseDouble(termsI[j+1]);
+				}else if(opsI[j].equals("/")) {
+					finalValue/=Double.parseDouble(termsI[j+1]);
+				}
+			}
+			addsubTerms.add(finalValue);
 		}
-		/*System.out.println("addsubTerms:");
-		for(Double d:addsubTerms) {
-			System.out.println(d);
-		}*/
 		boolean test2 = Arrays.stream(ops).anyMatch("*"::equals);
 		boolean test3 = Arrays.stream(ops).anyMatch("/"::equals);
 		//remove * and / from ops
@@ -83,27 +90,5 @@ public class Calculator {
 			}
 		}
 		//System.out.println(this.answer);
-	}
-	
-
-	private double multiply(double a,double b) {
-		return a*b;
-	}
-
-	private double divide(double a,double b) {
-		if(b==0.0) {
-			throw new ArithmeticException();
-		}
-		return a/b;
-	}
-	
-	@SuppressWarnings("unused")
-	private double add(double a,double b) {
-		return a+b;
-	}
-	
-	@SuppressWarnings("unused")
-	private double subtract(double a,double b) {
-		return a-b;
 	}
 }
--- src/test/java/nodatabase/test/TestApplicationTests.java
+++ src/test/java/nodatabase/test/TestApplicationTests.java
@@ -13,14 +13,20 @@ class TestApplicationTests {
 
 	@Test
 	void contextLoads() {
-		Calculator c = new Calculator("5*5-3*3+3*4");
-		assertEquals(28,c.getAnswer());
 		Calculator c2 = new Calculator("5*5-3/4");
 		assertEquals(24.25,c2.getAnswer());
+		Calculator c = new Calculator("5*5*5-3*3+3*4");
+		assertEquals(128,c.getAnswer());
 		Calculator c3 = new Calculator("1+1+1+1+1+1");
 		assertEquals(6,c3.getAnswer());
 		Calculator c4 = new Calculator("5*5-3");
 		assertEquals(22,c4.getAnswer());
+		Calculator c5 = new Calculator("5*5*5");
+		assertEquals(125, c5.getAnswer());
+		Calculator c6 = new Calculator("5*4*3*2*1");
+		assertEquals(120,c6.getAnswer());
+		Calculator c7 = new Calculator("5*4*3/2*1");
+		assertEquals(30,c7.getAnswer());
 	}
 
 }
