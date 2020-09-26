89069c3b58c5da5e32abbc7b133070b23687034f
Daan Snoeken <d.a.snoeken@protonmail.com>
2020-09-26 13:45:21 +0200

    hyperbolic functions

--- src/main/java/nodatabase/test/domain/Calculator.java
+++ src/main/java/nodatabase/test/domain/Calculator.java
@@ -32,7 +32,7 @@ public class Calculator {
 		this.errorMessage = "None";
 		this.answer = 0.0;
 		try {
-			if((countCharOcc(this.sum, '(') != countCharOcc(this.sum, ')'))){
+			if ((countCharOcc(this.sum, '(') != countCharOcc(this.sum, ')'))) {
 				throw new CalculatorSyntaxException("Syntax error! Parenthesis error!");
 			}
 			while (this.sum.contains("(") && (countCharOcc(this.sum, '(') == countCharOcc(this.sum, ')'))) {
@@ -50,29 +50,29 @@ public class Calculator {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void solveParenthesis() throws CalculatorSyntaxException {
 		int ind = this.sum.lastIndexOf('(');
-		//find next close parenthesis
+		// find next close parenthesis
 		int indP = -1;
-		for(int i=ind+1;i<this.sum.length();i++) {
-			if(this.sum.charAt(i) == ')') {
-				indP=i;
+		for (int i = ind + 1; i < this.sum.length(); i++) {
+			if (this.sum.charAt(i) == ')') {
+				indP = i;
 				break;
 			}
 		}
-		if(indP<0) {
+		if (indP < 0) {
 			throw new CalculatorSyntaxException("Parenthesis Error!");
 		}
-		String subsum = this.sum.substring(ind+1,indP);
-		//System.out.println(subsum+" = "+calculate(subsum));
+		String subsum = this.sum.substring(ind + 1, indP);
+		// System.out.println(subsum+" = "+calculate(subsum));
 		Double subAnswer = calculate(subsum);
 		StringBuilder replacer = new StringBuilder(this.sum);
-		replacer.replace(ind, indP+1, Double.toString(subAnswer));
+		replacer.replace(ind, indP + 1, Double.toString(subAnswer));
 		this.sum = replacer.toString();
-		System.out.println(this.sum);
+		// System.out.println(this.sum);
 	}
-	
+
 	public void calculate() {
 		this.sum = this.sum.replaceAll("\\s", ""); // remove spaces
 		this.terms = this.sum.split("\\+|\\-"); // contains numbers
@@ -90,32 +90,53 @@ public class Calculator {
 		}
 		// multiply and divide first
 		for (int i = 0; i < terms.length; i++) {
-			if (!terms[i].contains("*") && !terms[i].contains("/") && !terms[i].contains("^") && !checkSpecialFunction(terms[i])) {
+			if (!terms[i].contains("*") && !terms[i].contains("/") && !terms[i].contains("^")
+					&& !checkSpecialFunction(terms[i])) {
 				addsubTerms.add(Double.parseDouble(terms[i]));
 				continue;
 			}
 			String[] termsI = terms[i].split("\\*|\\/|\\^");
 			String[] opsI = terms[i].split("\\d{1,}\\.?\\d{0,}|[a-z]{1,}\\{\\d{1,}\\}");
-			for(int j = 0;j<termsI.length;j++) {
-				if (termsI[j].contains("sqrt")) {
+			for (int j = 0; j < termsI.length; j++) {
+				if (termsI[j].contains("sqrt")) { // square roots
 					String[] termsIp = termsI[j].split("\\{");
-					termsIp[1] = termsIp[1].substring(0, termsIp[1].length()-1); //contains number we want sqrt of
+					termsIp[1] = termsIp[1].substring(0, termsIp[1].length() - 1); // contains number we want sqrt of
 					termsI[j] = Double.toString(Math.sqrt(Double.valueOf(termsIp[1])));
 				}
-				if(termsI[j].contains("sin")) {
+				if (termsI[j].contains("cbrt")) {
 					String[] termsIp = termsI[j].split("\\{");
-					termsIp[1] = termsIp[1].substring(0, termsIp[1].length()-1); 
-					termsI[j] = Double.toString(Math.sin(Double.valueOf(termsIp[1])*Math.PI/180.0));
+					termsIp[1] = termsIp[1].substring(0, termsIp[1].length() - 1);
+					termsI[j] = Double.toString(Math.cbrt(Double.valueOf(termsIp[1])));
 				}
-				if(termsI[j].contains("cos")) {
+				if (termsI[j].contains("sin") && !termsI[j].contains("sinh")) { // trigonometry
 					String[] termsIp = termsI[j].split("\\{");
-					termsIp[1] = termsIp[1].substring(0, termsIp[1].length()-1); 
-					termsI[j] = Double.toString(Math.cos(Double.valueOf(termsIp[1])*Math.PI/180.0));
+					termsIp[1] = termsIp[1].substring(0, termsIp[1].length() - 1);
+					termsI[j] = Double.toString(Math.sin(Double.valueOf(termsIp[1]) * Math.PI / 180.0));
 				}
-				if(termsI[j].contains("tan")) {
+				if (termsI[j].contains("cos") && !termsI[j].contains("cosh")) {
 					String[] termsIp = termsI[j].split("\\{");
-					termsIp[1] = termsIp[1].substring(0, termsIp[1].length()-1); 
-					termsI[j] = Double.toString(Math.tan(Double.valueOf(termsIp[1])*Math.PI/180.0));
+					termsIp[1] = termsIp[1].substring(0, termsIp[1].length() - 1);
+					termsI[j] = Double.toString(Math.cos(Double.valueOf(termsIp[1]) * Math.PI / 180.0));
+				}
+				if (termsI[j].contains("tan") && !termsI[j].contains("tanh")) {
+					String[] termsIp = termsI[j].split("\\{");
+					termsIp[1] = termsIp[1].substring(0, termsIp[1].length() - 1);
+					termsI[j] = Double.toString(Math.tan(Double.valueOf(termsIp[1]) * Math.PI / 180.0));
+				}
+				if (termsI[j].contains("sinh")) { // hyperbolic functions
+					String[] termsIp = termsI[j].split("\\{");
+					termsIp[1] = termsIp[1].substring(0, termsIp[1].length() - 1);
+					termsI[j] = Double.toString(Math.sinh(Double.valueOf(termsIp[1])));
+				}
+				if (termsI[j].contains("cosh")) {
+					String[] termsIp = termsI[j].split("\\{");
+					termsIp[1] = termsIp[1].substring(0, termsIp[1].length() - 1);
+					termsI[j] = Double.toString(Math.cosh(Double.valueOf(termsIp[1])));
+				}
+				if (termsI[j].contains("tanh")) {
+					String[] termsIp = termsI[j].split("\\{");
+					termsIp[1] = termsIp[1].substring(0, termsIp[1].length() - 1);
+					termsI[j] = Double.toString(Math.tanh(Double.valueOf(termsIp[1])));
 				}
 			}
 			Double finalValue = Double.parseDouble(termsI[0]);
@@ -130,14 +151,18 @@ public class Calculator {
 				opsI = help.toArray(opsI);
 			}
 			for (int j = 0; j < opsI.length; j++) {
-				 if(opsI[j].equals("^")) {
+				if (opsI[j].equals("^")) {
 					double power = Double.parseDouble(termsI[j + 1]);
-					for(int k = 1; k<power;k++) {
-						finalValue *= Double.parseDouble(termsI[j]);
+					if ((int) power == power) {
+						for (int k = 1; k < power; k++) {
+							finalValue *= Double.parseDouble(termsI[j]);
+						}
+					} else {
+						finalValue = Math.pow(Double.parseDouble(termsI[j]), power);
 					}
-				}else if (opsI[j].equals("*")) {
+				} else if (opsI[j].equals("*")) {
 					finalValue *= Double.parseDouble(termsI[j + 1]);
-				}else if (opsI[j].equals("/")) {
+				} else if (opsI[j].equals("/")) {
 					finalValue /= Double.parseDouble(termsI[j + 1]);
 				}
 			}
@@ -145,11 +170,12 @@ public class Calculator {
 		}
 		boolean test2 = Arrays.stream(ops).anyMatch("*"::equals);
 		boolean test3 = Arrays.stream(ops).anyMatch("/"::equals);
-		// remove * and / from ops
-		if (test2 || test3) {
+		boolean test4 = Arrays.stream(ops).anyMatch("."::equals);
+		// remove *, . and / from ops
+		if (test2 || test3 || test4) {
 			ArrayList<String> help = new ArrayList<>();
 			for (int i = 0; i < ops.length; i++) {
-				if (!ops[i].equals("*") && !ops[i].equals("/"))
+				if (!ops[i].equals("*") && !ops[i].equals("/") && !ops[i].equals("."))
 					help.add(ops[i]);
 			}
 			ops = new String[help.size()];
@@ -164,9 +190,8 @@ public class Calculator {
 				this.answer -= addsubTerms.get(i + 1);
 			}
 		}
-		// System.out.println(this.answer);
 	}
-	
+
 	public Double calculate(String subsum) {
 		subsum = subsum.replace("\\s", ""); // remove spaces
 		String[] terms = subsum.split("\\+|\\-"); // contains numbers
@@ -185,32 +210,53 @@ public class Calculator {
 		}
 		// multiply and divide first
 		for (int i = 0; i < terms.length; i++) {
-			if (!terms[i].contains("*") && !terms[i].contains("/") && !terms[i].contains("^") && !checkSpecialFunction(terms[i])) {
+			if (!terms[i].contains("*") && !terms[i].contains("/") && !terms[i].contains("^")
+					&& !checkSpecialFunction(terms[i])) {
 				addsubTerms.add(Double.parseDouble(terms[i]));
 				continue;
 			}
 			String[] termsI = terms[i].split("\\*|\\/|\\^");
 			String[] opsI = terms[i].split("\\d{1,}\\.?\\d{0,}|[a-z]{1,}\\{\\d{1,}\\}");
-			for(int j = 0;j<termsI.length;j++) {
+			for (int j = 0; j < termsI.length; j++) {
 				if (termsI[j].contains("sqrt")) {
 					String[] termsIp = termsI[j].split("\\{");
-					termsIp[1] = termsIp[1].substring(0, termsIp[1].length()-1); //contains number we want sqrt of
+					termsIp[1] = termsIp[1].substring(0, termsIp[1].length() - 1); // contains number we want sqrt of
 					termsI[j] = Double.toString(Math.sqrt(Double.valueOf(termsIp[1])));
 				}
-				if(termsI[j].contains("sin")) {
+				if (termsI[j].contains("cbrt")) {
 					String[] termsIp = termsI[j].split("\\{");
-					termsIp[1] = termsIp[1].substring(0, termsIp[1].length()-1); 
-					termsI[j] = Double.toString(Math.sin(Double.valueOf(termsIp[1])*Math.PI/180.0));
+					termsIp[1] = termsIp[1].substring(0, termsIp[1].length() - 1);
+					termsI[j] = Double.toString(Math.cbrt(Double.valueOf(termsIp[1])));
 				}
-				if(termsI[j].contains("cos")) {
+				if (termsI[j].contains("sin") && !termsI[j].contains("sinh")) {
 					String[] termsIp = termsI[j].split("\\{");
-					termsIp[1] = termsIp[1].substring(0, termsIp[1].length()-1); 
-					termsI[j] = Double.toString(Math.cos(Double.valueOf(termsIp[1])*Math.PI/180.0));
+					termsIp[1] = termsIp[1].substring(0, termsIp[1].length() - 1);
+					termsI[j] = Double.toString(Math.sin(Double.valueOf(termsIp[1]) * Math.PI / 180.0));
 				}
-				if(termsI[j].contains("tan")) {
+				if (termsI[j].contains("cos") && !termsI[j].contains("cosh")) {
 					String[] termsIp = termsI[j].split("\\{");
-					termsIp[1] = termsIp[1].substring(0, termsIp[1].length()-1); 
-					termsI[j] = Double.toString(Math.tan(Double.valueOf(termsIp[1])*Math.PI/180.0));
+					termsIp[1] = termsIp[1].substring(0, termsIp[1].length() - 1);
+					termsI[j] = Double.toString(Math.cos(Double.valueOf(termsIp[1]) * Math.PI / 180.0));
+				}
+				if (termsI[j].contains("tan") && !termsI[j].contains("tanh")) {
+					String[] termsIp = termsI[j].split("\\{");
+					termsIp[1] = termsIp[1].substring(0, termsIp[1].length() - 1);
+					termsI[j] = Double.toString(Math.tan(Double.valueOf(termsIp[1]) * Math.PI / 180.0));
+				}
+				if (termsI[j].contains("sinh")) { // hyperbolic functions
+					String[] termsIp = termsI[j].split("\\{");
+					termsIp[1] = termsIp[1].substring(0, termsIp[1].length() - 1);
+					termsI[j] = Double.toString(Math.sinh(Double.valueOf(termsIp[1])));
+				}
+				if (termsI[j].contains("cosh")) {
+					String[] termsIp = termsI[j].split("\\{");
+					termsIp[1] = termsIp[1].substring(0, termsIp[1].length() - 1);
+					termsI[j] = Double.toString(Math.cosh(Double.valueOf(termsIp[1])));
+				}
+				if (termsI[j].contains("tanh")) {
+					String[] termsIp = termsI[j].split("\\{");
+					termsIp[1] = termsIp[1].substring(0, termsIp[1].length() - 1);
+					termsI[j] = Double.toString(Math.tanh(Double.valueOf(termsIp[1])));
 				}
 			}
 			Double finalValue = Double.parseDouble(termsI[0]);
@@ -225,14 +271,18 @@ public class Calculator {
 				opsI = help.toArray(opsI);
 			}
 			for (int j = 0; j < opsI.length; j++) {
-				if(opsI[j].equals("^")) {
+				if (opsI[j].equals("^")) {
 					double power = Double.parseDouble(termsI[j + 1]);
-					for(int k = 1; k<power;k++) {
-						finalValue *= Double.parseDouble(termsI[j]);
+					if ((int) power == power) {
+						for (int k = 1; k < power; k++) {
+							finalValue *= Double.parseDouble(termsI[j]);
+						}
+					} else {
+						finalValue = Math.pow(Double.parseDouble(termsI[j]), power);
 					}
-				}else if (opsI[j].equals("*")) {
+				} else if (opsI[j].equals("*")) {
 					finalValue *= Double.parseDouble(termsI[j + 1]);
-				}else if (opsI[j].equals("/")) {
+				} else if (opsI[j].equals("/")) {
 					finalValue /= Double.parseDouble(termsI[j + 1]);
 				}
 			}
@@ -240,11 +290,12 @@ public class Calculator {
 		}
 		boolean test2 = Arrays.stream(ops).anyMatch("*"::equals);
 		boolean test3 = Arrays.stream(ops).anyMatch("/"::equals);
-		// remove * and / from ops
-		if (test2 || test3) {
+		boolean test4 = Arrays.stream(ops).anyMatch("."::equals);
+		// remove *, . and / from ops
+		if (test2 || test3 || test4) {
 			ArrayList<String> help = new ArrayList<>();
 			for (int i = 0; i < ops.length; i++) {
-				if (!ops[i].equals("*") && !ops[i].equals("/"))
+				if (!ops[i].equals("*") && !ops[i].equals("/") && !ops[i].equals("."))
 					help.add(ops[i]);
 			}
 			ops = new String[help.size()];
@@ -261,7 +312,7 @@ public class Calculator {
 		}
 		return answer;
 	}
-	
+
 	private boolean syntaxCheck() {
 		if (this.sum.contains("**") || this.sum.contains("//") || this.sum.contains("++") || this.sum.contains("--")) {
 			return false;
@@ -282,11 +333,14 @@ public class Calculator {
 		}
 		return ans;
 	}
+
 	private boolean checkSpecialFunction(String input) {
-		if(input.contains("sqrt") && input.contains("sin") && input.contains("cos") && input.contains("tan")) {
-			return false;
-		}else {
+		if (input.contains("sqrt") || input.contains("sin") || input.contains("cos") || input.contains("tan")
+				|| input.contains("cbrt") || input.contains("sinh") || input.contains("cosh")
+				|| input.contains("tanh")) {
 			return true;
+		} else {
+			return false;
 		}
 	}
 }
